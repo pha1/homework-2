b7a3ac4f3d644eaaef699edf3d7aa1c76690fe11
Phi <pha1@uncc.edu>
2022-09-07 18:04:23 -0400

    Version 1.0.2 Created View Drinks Activity Create Add Drinks Activity

---
+++ app/src/main/java/com/example/group9_hw1/AddDrinkActivity.java
@@ -0,0 +1,14 @@
+package com.example.group9_hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDrinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_drink);
+    }
+}
---
+++ app/src/main/java/com/example/group9_hw1/ViewDrinksActivity.java
@@ -0,0 +1,14 @@
+package com.example.group9_hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewDrinksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_drinks);
+    }
+}
