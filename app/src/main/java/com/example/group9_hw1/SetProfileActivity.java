d3f8f9705a9b850f9ae74dd92a6250a38fde1335
Phi <pha1@uncc.edu>
2022-09-07 17:58:55 -0400

    Version 1.0.1 Created SetProfile Activity

---
+++ app/src/main/java/com/example/group9_hw1/SetProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.group9_hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+    }
+}
