d7dc0ce5cfecea5647a6a94ce5a9988986bc4e04
sdittaka <sdittaka@uncc.edu>
2022-09-10 18:43:00 -0400

    Completed all the on Click methods and added the drinks arraylist.

--- app/src/main/java/com/example/group9_hw1/ViewDrinksActivity.java
+++ app/src/main/java/com/example/group9_hw1/ViewDrinksActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class ViewDrinksActivity extends AppCompatActivity {
 
     public static int numDrinks = 0;
     final public static String VIEW_DRINKS_KEY = "VIEW_DRINKS";
+    public ArrayList<Drink> drinks;
+    public int current = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,24 @@ public class ViewDrinksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_drinks);
         setTitle("View Drinks");
 
+        if(getIntent() != null && getIntent().getExtras() != null && getIntent().hasExtra(MainActivity.DRINKS_KEY)){
+            drinks = getIntent().getParcelableExtra(MainActivity.DRINKS_KEY);
+            drinks.get(current);
+        }
+
+
+
         // Click next to get the next drink in the ArrayList
         // If the current drink is the last drink then show the first drink next
         findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (drinks.get(current) == drinks.get(drinks.size()-1)){
+                    drinks.get(0);
+                }
+                else {
+                    drinks.get(current++);
+                }
             }
         });
 
@@ -29,7 +45,10 @@ public class ViewDrinksActivity extends AppCompatActivity {
         findViewById(R.id.trashButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (drinks != null){
+                    drinks.remove(current);
+                }
+                drinks.get(current--);
             }
         });
 
@@ -38,7 +57,14 @@ public class ViewDrinksActivity extends AppCompatActivity {
         findViewById(R.id.previousButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(drinks != null){
+                    if(drinks.get(current) == drinks.get(0)){
+                        drinks.get(drinks.size()-1);
+                    }
+                }
+                else{
+                    drinks.get(current--);
+                }
             }
         });
 
